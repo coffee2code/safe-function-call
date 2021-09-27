97d815ade3e0c1be71bafef64b9c40d03aa963ec
Scott Reilly <scott@coffee2code.com>
2021-09-26 23:54:44 -0700

    Unit tests: Restructure unit test directories
    * Move `phpunit/bin/` into `tests/`
    * Move `phpunit/` into `tests/`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 ## _(in-progress)_
 * Change: Note compatibility through WP 5.8+
+* Unit tests:
+    * Change: Restructure unit test directories
+        * Change: Move `phpunit/bin/` into `tests/`
+        * Change: Move `phpunit/` into `tests/`
 
 ## 1.3 _(2021-04-17)_
 
--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -20,7 +20,7 @@ require_once $_tests_dir . '/tests/phpunit/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/safe-function-call.php';
+	require dirname( __FILE__, 3 ) . '/safe-function-call.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
